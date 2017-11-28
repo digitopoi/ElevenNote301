cd2e8c58122752961bcaaae6cd0674ee9005f33a
digitopoi <hubo85@gmail.com>
2017-11-28 13:39:38 -0600

    Initial commit

---
+++ ElevenNote.Models/NoteCreate.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Models
+{
+    public class NoteCreate
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+
+        public override string ToString() => Title;
+
+    }
+}
