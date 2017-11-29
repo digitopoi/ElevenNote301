b734a4fc928187337194f7e48b54b92f103f668e
digitopoi <hubo85@gmail.com>
2017-11-29 17:24:41 -0600

    Add dependencies

--- ElevenNoteMobileApp/ElevenNoteMobileApp/App.xaml.cs
+++ ElevenNoteMobileApp/ElevenNoteMobileApp/App.xaml.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using ElevenNoteMobileApp.Contracts;
+using ElevenNoteMobileApp.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 using Xamarin.Forms;
 
+//  Define dependencies for injection
+#if DEBUG
+[assembly: Dependency(typeof(FakeNoteService))]
+#else
+[assembly: Dependency(typeof(RealNoteService))]
+#endif
 namespace ElevenNoteMobileApp
 {
+    
 	public partial class App : Application
 	{
+        public static readonly INoteService NoteService = DependencyService.Get<INoteService>();
+
 		public App ()
 		{
 			InitializeComponent();
---
+++ ElevenNoteMobileApp/ElevenNoteMobileApp/Services/FakeNoteService.cs
@@ -0,0 +1,42 @@
+﻿using ElevenNoteMobileApp.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using ElevenNote.Models;
+using System.Threading.Tasks;
+
+namespace ElevenNoteMobileApp.Services
+{
+    public class FakeNoteService : INoteService
+    {
+        public Task<bool> AddNew(NoteCreate note)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<NoteListItem>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<NoteDetail> GetById(int noteId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Login(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Update(NoteEdit note)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
