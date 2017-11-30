083dde112cd186ebf450ab992ec34ca2ee50b9a0
digitopoi <hubo85@gmail.com>
2017-11-30 09:07:48 -0600

    Add fake note service

--- ElevenNoteMobileApp/ElevenNoteMobileApp/Services/FakeNoteService.cs
+++ ElevenNoteMobileApp/ElevenNoteMobileApp/Services/FakeNoteService.cs
@@ -4,39 +4,72 @@ using System.Collections.Generic;
 using System.Text;
 using ElevenNote.Models;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace ElevenNoteMobileApp.Services
 {
     public class FakeNoteService : INoteService
     {
-        public Task<bool> AddNew(NoteCreate note)
+        private List<NoteDetail> Notes { get; set; } = new List<NoteDetail>();
+
+        public async Task<bool> Login(string username, string password)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
-        public Task<bool> Delete(int noteId)
+        public async Task<List<NoteListItem>> GetAll()
         {
-            throw new NotImplementedException();
+            return Notes.Select(note => new NoteListItem
+            {
+                NoteId = note.NoteId,
+                Content = note.Content,
+                CreatedUtc = note.CreatedUtc,
+                IsStarred = false,
+                Title = note.Title
+            }).OrderBy(o => o.CreatedUtc).ToList();
         }
 
-        public Task<List<NoteListItem>> GetAll()
+        public async Task<NoteDetail> GetById(int noteId)
         {
-            throw new NotImplementedException();
+            return Notes.FirstOrDefault(note => note.NoteId == noteId);
         }
 
-        public Task<NoteDetail> GetById(int noteId)
+        public async Task<bool> AddNew(NoteCreate note)
         {
-            throw new NotImplementedException();
+            if (note.Title == string.Empty) return false;
+
+            Notes.Add(new NoteDetail
+            {
+                NoteId = Notes.Any() ? Notes.Max(n => n.NoteId) + 1 : 1,
+                Content = note.Content,
+                Title = note.Title,
+                CreatedUtc = DateTimeOffset.UtcNow
+            });
+
+            return true;
         }
 
-        public Task<bool> Login(string username, string password)
+        public async Task<bool> Update(NoteEdit note)
         {
-            throw new NotImplementedException();
+            var noteToUpdate = await GetById(note.NoteId);
+
+            if (noteToUpdate == null) return false;
+
+            noteToUpdate.Title = note.Title;
+            noteToUpdate.Content = note.Content;
+            noteToUpdate.ModifiedUtc = DateTimeOffset.UtcNow;
+
+            return true;
         }
 
-        public Task<bool> Update(NoteEdit note)
+        public async Task<bool> Delete(int noteId)
         {
-            throw new NotImplementedException();
+            var noteToUpdate = await GetById(noteId);
+
+            if (noteToUpdate == null) return false;
+
+            Notes.Remove(noteToUpdate);
+            return true;
         }
     }
 }
