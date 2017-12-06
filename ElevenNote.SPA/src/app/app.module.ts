96c2880219829dc0f2539b3fc063113a4187e1fc
digitopoi <hubo85@gmail.com>
2017-12-06 12:24:57 -0600

    Restart with docs

--- ElevenNote.SPA/src/app/app.module.ts
+++ ElevenNote.SPA/src/app/app.module.ts
@@ -1,58 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
-// Services
-import { AuthService } from './services/auth.service';
 
-// Material Components
-import 'hammerjs';
-import { MatToolbarModule,
-         MatButtonModule,
-         MatFormFieldModule,
-         MatInputModule } from '@angular/material';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
-// Components
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { RegisterComponent } from './components/register/register.component';
-import { LoginComponent } from './components/login/login.component';
-import { NoteIndexComponent } from './components/notes/note-index/note-index.component';
-
-const routes = [
-  { path: 'home', component: AppComponent},
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
-  { path: '**', component: RegisterComponent }
-];
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RegisterComponent,
-    LoginComponent,
-    NoteIndexComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    RouterModule.forRoot(routes),
-    FormsModule,
-    ReactiveFormsModule,
-    HttpModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatFormFieldModule,
-    MatInputModule
+    BrowserModule
   ],
-  providers: [
-    AuthService
-  ],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
