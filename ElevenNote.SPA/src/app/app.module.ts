6ee9be5f1ae1ec4f733f5ffeb640e4f6fe99c042
digitopoi <hubo85@gmail.com>
2017-12-05 23:02:03 -0600

    Initial setup

--- ElevenNote.SPA/src/app/app.module.ts
+++ ElevenNote.SPA/src/app/app.module.ts
@@ -1,7 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//  Services
+import { AuthService } from './services/auth.service';
 
+//  Material Components
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material';
+import 'hammerjs';
+
+//  Components
 import { AppComponent } from './app.component';
 
 
