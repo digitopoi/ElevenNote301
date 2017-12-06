4ea21ef3802092ef9f2041ba9b0705f6346f3f0b
digitopoi <hubo85@gmail.com>
2017-12-06 13:11:28 -0600

    Add registration component

--- ElevenNote.SPA/src/app/app.module.ts
+++ ElevenNote.SPA/src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatToolbarModule,
-         MatButtonModule } from '@angular/material';
+import {
+         MatToolbarModule,
+         MatButtonModule
+        } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { RegistrationComponent } from './components/registration/registration.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RegistrationComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     MatToolbarModule,
     MatButtonModule
   ],
