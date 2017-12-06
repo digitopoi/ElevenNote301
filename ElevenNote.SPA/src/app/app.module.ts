cecc17222710fcc81acbf4f16a86cb2e73bc167d
digitopoi <hubo85@gmail.com>
2017-12-06 13:56:25 -0600

    Add routing

--- ElevenNote.SPA/src/app/app.module.ts
+++ ElevenNote.SPA/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import {
          MatToolbarModule,
@@ -8,11 +9,16 @@ import {
          MatFormFieldModule,
          MatInputModule
         } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { RegistrationComponent } from './components/registration/registration.component';
 
+const routes = [
+  { path: 'register', component: RegistrationComponent },
+  { path: '**', component: RegistrationComponent }
+];
 
 @NgModule({
   declarations: [
@@ -23,10 +29,12 @@ import { RegistrationComponent } from './components/registration/registration.co
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(routes),
     ReactiveFormsModule,
     MatToolbarModule,
     MatFormFieldModule,
-    MatButtonModule, 
+    MatButtonModule,
+    BrowserAnimationsModule,
     MatInputModule
   ],
   providers: [],
